96c5dd5454d7a2f66244001ffe671989710f3c27
andrey.sendme@gmail.com <andrey.sendme@gmail.com@16b62102-acdd-4989-265f-d42817786e9b>
2013-06-29 05:01:06 +0000

    Added in structure for gui (yay copying from past work!)

--- trunk/GeneticAnimals/src/GeneticAnimal.java
+++ trunk/GeneticAnimals/src/GeneticAnimal.java
@@ -1,4 +1,24 @@
+import java.util.List;
+
 
 public abstract class GeneticAnimal {
+	
+	protected double life;
+	protected boolean alive;
+	protected double speed;
+	protected double size;
+	
+	public GeneticAnimal(int life){
+		this.life=life;
+		alive=true;
+	}
+	
+	public boolean isAlive(){
+		return alive;
+	}
+	
+	public abstract List<GeneticAnimal> update();
+	
+	public abstract boolean isPrey();//We have two species
 
 }
